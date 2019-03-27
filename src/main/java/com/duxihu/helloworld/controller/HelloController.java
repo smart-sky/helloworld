a106678b5fc3b42e33d9b104a51949856c5adfb8
xianfuhao <xianfuhao@do1.com.cn>
2019-03-27 15:43:53 +0800

    Initial commit

---
+++ src/main/java/com/duxihu/helloworld/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.duxihu.helloworld.controller;
+
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/helloworld")
+    public String HelloWorld(){
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/duxihu/helloworld/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.duxihu.helloworld.repository;
+
+import com.duxihu.helloworld.domain.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    User saveOrUpdateUser (User user);//新增或者修改用户
+
+    void deleteUsere(Long id); //删除用户
+
+    User getUserById(Long id); //根据用户id获取用户
+
+    List<User> userList (); //获取所有用户的列表
+}
